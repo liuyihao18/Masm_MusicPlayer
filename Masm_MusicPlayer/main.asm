f0faea5fb64ca86346227f84d6bef2f5a2b5ff34
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2021-10-17 11:23:26 +0800

    add GetWavToBuffer

--- Masm_MusicPlayer/main.asm
+++ Masm_MusicPlayer/main.asm
@@ -1,12 +1,14 @@
 ; main.asm
 INCLUDE     custom.inc
+INCLUDE     music_api.inc
 
 .data
-filename    BYTE            "1.mp3", 0
+filename    BYTE            "1.wav", 0
 hFile       DWORD           ?
 
 .code
 main PROC
+    INVOKE  PlayMusic,  OFFSET filename
     INVOKE  ExitProcess, 0
 main ENDP
 
--- Masm_MusicPlayer/music_api.asm
+++ Masm_MusicPlayer/music_api.asm
@@ -3,6 +3,7 @@
 INCLUDE custom.inc
 INCLUDE music_api.inc
 INCLUDE util.inc
+INCLUDE c.inc
 
 .data
 ; 参数
@@ -23,10 +24,13 @@ mutexPlaying    HANDLE      0               ; 允许播放状态互斥量
 mutexIsPlaying  HANDLE      0               ; 正在播放状态互斥量
 canPlaying      HANDLE      0               ; 播放权
 
+; 字符串常量
+eventDescript   BYTE        "PCM WRITE", 0  ; 消息描述
+
 .code
 
 ; 格式说明
-; WAVEFORMATEX{
+; WAVEFORMATEX {
 ;     WORD  wFormatTag,  // 波形-音频格式类型：PCM
 ;     WORD  nChannels,  // 声道数
 ;     DWORD nSamplesPerSec,  // 采样频率
@@ -38,7 +42,7 @@ canPlaying      HANDLE      0               ; 播放权
 GetWavFormat PROC USES ecx edx esi edi,
             hFile:                  HANDLE,                 ; 文件句柄
             format:                 PTR WAVEFORMATEX        ; 指向结构体的指针
-    LOCAL   buffer[WAV_HEAD_SIZE]:  BYTE                    ; 读取文件的Buffer
+    LOCAL   buffer[WAV_HEAD_SIZE]:  BYTE,                   ; 读取文件的Buffer
             realRead:               DWORD                   ; 实际读取的字节数
 ;   RETURN: BOOL 
     lea     esi, buffer
@@ -59,7 +63,7 @@ GetWavFormat PROC USES ecx edx esi edi,
     mov     edi, format
     mov     ecx, SIZEOF WAVEFORMATEX
     cld
-    rep     stob
+    rep     stosb
     ; 结构体填充
     add     esi, 20
     mov     edi, format
@@ -75,31 +79,174 @@ right:
     ret
 GetWavFormat ENDP
 
-_PlayMusic PROC USES ebx,
-    filename: PTR BYTE,             ; 文件名
-    musicType: DWORD                ; 音乐类型
-    LOCAL hFile: HANDLE,            ; 文件句柄
-        waveFormat: WAVEFORMATEX    ; 音乐格式
-    INVOKE  CreateFile,
-        ADDR filename,          ; LSCPTR: 指向文件名的指针
-        GENERIC_READ,           ; DWORD: 访问模式
-        FILE_SHARE_READ,        ; DWORD: 共享模式
-        NULL,                   ; LPSECURITY_ATTRIBUTES: 指向安全属性的指针
-        OPEN_EXISTING,          ; DWORD: 创建方式
-        FILE_ATTRIBUTE_NORMAL,  ; DWORD: 文件属性
-        NULL                    ; HANDLE: 用于复制文件句柄
-    cmp     eax, INVALID_HANDLE_VALUE
-    je      quit
-    mov     hFile, eax
-    INVOKE  GetMp3Format,
-        ADDR filename, 
-        ADDR waveFormat
-    cmp     eax, FALSE
-    je      quit
-    mov     ebx, eax
+GetWavToBuffer PROC USES ecx edx edi,
+            hFile:              HANDLE,         ; 文件句柄
+            musicBufferSize:    PTR DWORD       ; 指向音乐缓冲区大小的指针
+    LOCAL   musicSize:          DWORD,          ; 音乐文件大小
+            musicBuffer:        PTR BYTE,       ; 指向音乐缓冲区的指针
+            realRead:           DWORD           ; 实际读取的字节数
+;   RETURN: PTR BYTE
+    INVOKE  GetFileSize,
+            hFile,
+            NULL 
+    cmp     eax, INVALID_FILE_SIZE
+    je      wrong
+    mov     musicSize, eax
+    INVOKE  malloc, musicSize                   ; 动态申请内存
+    cmp     eax, NULL
+    je      wrong
+    mov     musicBuffer, eax
+    ; 内存清空
+    mov     al, 0
+    mov     edi, musicBuffer
+    mov     ecx, musicSize
+    cld
+    rep     stosb
+    ; 读取文件
+    lea     edx, realRead
+    sub     musicSize, WAV_HEAD_SIZE
+    INVOKE  ReadFile,
+            hFile,
+            musicBuffer,
+            musicSize,
+            edx,
+            NULL
+    cmp     eax, 0
+    je      freeMemory
+    mov     eax, realRead
+    cmp     eax, musicSize
+    jb      freeMemory
+    jmp     right
+freeMemory:
+    INVOKE  free, musicBuffer
+wrong:
+    mov     eax, NULL
+    ret
+right:
+    mov     eax, realRead
+    mov     edi, musicBufferSize
+    mov     [edi], eax
+    mov     eax, musicBuffer
+    ret
+GetWavToBuffer ENDP
 
-quit:
+GetMinBufferSize PROC USES ebx edx,
+            format:     WAVEFORMATEX        ; 文件格式
+;   RETURN: DWORD
+    mov     eax, 64
+    mul     format.nChannels
+    mul     format.wBitsPerSample
+    mul     format.nSamplesPerSec
+    mov     edx, 0
+    mov     ebx, 11025
+    div     ebx
+    ret
+GetMinBufferSize ENDP
+
+_PlayMusic PROC USES edx esi edi,
+            filename:       PTR BYTE,           ; 文件名
+            musicType:      DWORD               ; 音乐类型
+    LOCAL   hFile:          HANDLE,             ; 文件句柄
+            musicBuffer:    PTR BYTE,           ; 音乐缓冲区
+            musicSize:      DWORD,              ; 音乐大小
+            waveFormat:     WAVEFORMATEX,       ; 音乐格式
+            hEvent:         HANDLE              ; 回调事件句柄
+
+    ; 准备播放
+    mov     Playing, FALSE
+
+    ; 处理文件
+    INVOKE  CreateFile,
+            filename,               
+            GENERIC_READ,           
+            FILE_SHARE_READ,        
+            NULL,                  
+            OPEN_EXISTING,          
+            FILE_ATTRIBUTE_NORMAL,  
+            NULL                    
+    cmp     eax, INVALID_HANDLE_VALUE
+    je      wrong
+    mov     hFile, eax
+
+    ; 处理音频文件
+    cmp     musicType, WAV
+    je      wav
+    cmp     musicType, MP3
+    je      mp3
+    jmp     closeFileHandle
+wav:
+    ; 获取音频你格式
+    lea     edi, waveFormat
+    INVOKE  GetWavFormat, hFile, edi
+    cmp     eax, FALSE
+    je      closeFileHandle
+    ; 获取音频内容
+    lea     edi, musicSize
+    INVOKE  GetWavToBuffer, hFile, edi
+    cmp     eax, NULL
+    je      closeFileHandle
+    mov     musicBuffer, eax
+    jmp     next
+mp3:
+    ; 获取音频你格式
+    lea     edi, waveFormat
+    INVOKE  GetMp3Format, filename, edi
+    cmp     eax, FALSE
+    je      closeFileHandle
+    ; 获取音频内容
+    lea     edi, musicSize
+    INVOKE  DecodeMp3ToBuffer, hFile, edi
+    cmp     eax, NULL
+    je      closeFileHandle
+    mov     musicBuffer, eax     
+next:
+    mov     eax, musicSize
+    mov     edx, 0
+    div     waveFormat.nAvgBytesPerSec
+    mov     totalTime, eax
+    
+    ; 创建回调事件
+    INVOKE  CreateEvent,
+            NULL,
+            FALSE,
+            FALSE,
+            ADDR eventDescript
+    cmp     eax, NULL
+    je      freeMemory
+    mov     hEvent, eax
+
+    ; 打开音频
+    lea     esi, waveFormat
+    INVOKE  waveOutOpen,
+            OFFSET hWaveOut,
+            WAVE_MAPPER,
+            esi,
+            hEvent,
+            NULL,
+            CALLBACK_EVENT
+    cmp     eax, MMSYSERR_NOERROR
+    jne     closeEventHandle
+    jmp     right
+
+
+closeEventHandle:
+    INVOKE  CloseHandle, hEvent
+freeMemory:
+    INVOKE  free, musicBuffer
+closeFileHandle:
+    INVOKE  CloseHandle, hFile
+wrong:
+    mov     eax, FALSE
     ret    
+right:
+    mov     eax, TRUE
+    ret
 _PlayMusic ENDP
+
+PlayMusic PROC USES ebx,
+    filename: PTR BYTE
+    INVOKE  _PlayMusic, filename, WAV
+    ret
+PlayMusic ENDP
 
 END
